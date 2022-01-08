6fe9736723572f6cc2fd0ffe80bf12286ba01c7f
NobertNagy <nagynorbert314@gmail.com>
2022-01-08 16:51:34 +0100

    Initial commit

---
+++ BookingBotRFT/Dialogs/Data/UserProfile.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookingBotRFT.Dialogs.Data
+{
+    public class UserProfile
+    {
+        private string hotel;
+
+        private string date;
+
+        private string name;
+
+        private string email;
+
+        public string Hotel
+        {
+            get { return hotel; }
+            set { hotel = value; }
+        }
+        public string Date
+        {
+            get { return date; }
+            set { date = value; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+        public UserProfile(string _hotel, string _date, string _name, string _email)
+        {
+            Hotel = _hotel;
+            Date = _date;
+            Name = _name;
+            Email = _email;
+        }
+    }
+}
---
+++ BookingBotRFT/Dialogs/MainDialog.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Dialogs.Choices;
+using Microsoft.Bot.Connector;
+using Microsoft.Bot.Schema;
+
+using BookingBotRFT.Dialogs.Data;
+
+namespace BookingBotRFT
+{
+    public class MainDialog: ComponentDialog
+    {
+        private readonly IStatePropertyAccessor<UserProfile> _userProfileAccessor;
+        public MainDialog(UserState userState) : base(nameof(MainDialog))
+        {
+            _userProfileAccessor = userState.CreateProperty<UserProfile>("UserProfile");
+        }
+    }
+}
